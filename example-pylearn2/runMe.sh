f411e73e0ae9467c0c64d211adbdda35f2601519
Junbo Zhang <lucktroy@users.noreply.github.com>
2014-04-26 16:35:02 +0800

    Update runMe.sh

--- example-pylearn2/runMe.sh
+++ example-pylearn2/runMe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export PYLEARN2_DATA_PATH=/tmp
 export PATH=$PATH:/opt/pylearn2/pylearn2/scripts
-export TESTPATH=/opt/Test/regression-deep-learning/example/pylearn2
-export PYTHONPATH=$PYTHONPATH:/opt/Test/regression-deep-learning/example/pylearn2
+export TESTPATH=/opt/Test/regression-deep-learning/example-pylearn2
+export PYTHONPATH=$PYTHONPATH:/opt/Test/regression-deep-learning/example-pylearn2
 train.py $TESTPATH/red_wine_quality.yaml
